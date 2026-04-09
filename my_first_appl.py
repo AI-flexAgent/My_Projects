07922da1e152b23e3468ec150b9f6db61ac17bf8
Pixel24777 <61551549+Pixel24777@users.noreply.github.com>
2026-04-09 22:19:07 +0300

    homework_1

---
+++ my_first_appl.py
@@ -0,0 +1,11 @@
+print("I am a programmer!")
+print("Hello i am <Valentin>")
+
+# --------------------------
+
+print("I am a programmer!\nHello i am <Valentin>")
+
+# --------------------------
+
+print("I", "am", "a", "programmer",sep=" ", end="!\n")
+print("Hello", "i", "am", "<Valentin>", sep=" ")
